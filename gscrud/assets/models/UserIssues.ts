cd3cec5f1e66f4c82304d865104df07c91a0708f
jidni911 <gidni441@gmail.com>
2025-09-22 09:58:03 +0600

    Auto-generated commit

---
+++ gscrud/assets/models/UserIssues.ts
@@ -0,0 +1,12 @@
+export interface UserIssues{
+    id: number;
+    status: string;
+    billDate: String;
+    customerName:String;
+    brand:String;
+    model:String;
+    serial:String;
+    bill:number;
+    repairDescription:String;
+    customerNameInPLO:String;
+}
